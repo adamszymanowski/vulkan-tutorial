95d5e38d74c884d8e54e61ac5dd0afac7be996e9
Adam Szymanowski <adam.szym@yahoo.pl>
2022-08-26 11:15:56 +0200

    Tutorial 03: Validation Layers (before adding debug info)

--- Windows/t03-validation-layers/main.cpp
+++ Windows/t03-validation-layers/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 class HelloTriangleApplication
 {
@@ -22,6 +23,15 @@ private:
 	const uint32_t
 		m_width = 800,
 		m_height = 600;
+	const std::vector<const char*> m_validation_layers = {
+		"VK_LAYER_KHRONOS_validation"
+	};
+
+#ifdef NDEBUG
+	const bool m_enable_validation_layers = false;
+#else
+	const bool m_enable_validation_layers = true;
+#endif
 
 	void initialize_window()
 	{
@@ -38,6 +48,11 @@ private:
 
 	void create_vulkan_instance()
 	{
+		if (m_enable_validation_layers and not check_validation_layer_support())
+		{
+			throw std::runtime_error("Validation layers requested, but not available!");
+		}
+
 		VkApplicationInfo application_info{};
 		application_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 		application_info.pApplicationName = "Hello Triangle";
@@ -56,12 +71,47 @@ private:
 
 		instance_info.enabledExtensionCount = glfw_extension_count;
 		instance_info.ppEnabledExtensionNames = glfw_extensions;
-		instance_info.enabledLayerCount = 0;
+		if (m_enable_validation_layers)
+		{
+			instance_info.enabledLayerCount = static_cast<uint32_t>(m_validation_layers.size());
+			instance_info.ppEnabledLayerNames = m_validation_layers.data();
+		}
+		else
+		{
+			instance_info.enabledLayerCount = 0;
+		}
 
 		if (vkCreateInstance(&instance_info, nullptr, &m_vulkan_instance) != VK_SUCCESS)
 		{
 			throw std::runtime_error("Failed to create Vulkan instance!");
 		}
+	}
+
+	bool check_validation_layer_support()
+	{
+		uint32_t layer_count;
+		vkEnumerateInstanceLayerProperties(&layer_count, nullptr); // get count for vector size
+		std::vector<VkLayerProperties> available_layers { layer_count };
+		vkEnumerateInstanceLayerProperties(&layer_count, available_layers.data());
+
+		for (const auto& layer_name : m_validation_layers)
+		{
+			bool layer_found = false;
+			for (const auto& layer_properties : available_layers)
+			{
+				if (strcmp(layer_name, layer_properties.layerName) == 0)
+				{
+					layer_found = true;
+					break;
+				}
+			}
+
+			if (not layer_found)
+			{
+				return false;
+			}
+		}
+		return true;
 	}
 
 	void main_loop()
